35739da6f3f074fa99913f469301c70e58d046c7
Normaljun95 <jsj950629@gmail.com>
2023-10-12 02:36:07 +0900

    🔨RESTAPI 적용1 및 CSS 디자인 추가 작업

--- advice-generator/src/AdviceCard.js
+++ advice-generator/src/AdviceCard.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import dividerDesktop from './images/pattern-divider-desktop.svg'
+import dividerMobile from './images/pattern-divider-mobile.svg'
 import dice from './images/icon-dice.svg'
+import axios from "axios"
 
 export const AdviceCard = () => {
+  const [advice, setAdvice] = useState("")
+  const getAdvice = async() => {
+    const response = await axios.get("https://api.adviceslip.com");
+    console.log(response)
+  }
+
+  useEffect(() => {
+    getAdvice()
+  }, [])
   return (
     <div className='card'>
         <p>ADVISE #1</p>
         <h2>This is an advice</h2>
         <img src={dividerDesktop} className='divider-desktop' alt='divider' />
-        <div className='dice'>
+        <img src={dividerMobile} className='divider-mobile' alt='divider' />
+        <div className='dice' onClick={getAdvice}>
             <img src={dice} alt='dice' />
         </div>
     </div>
